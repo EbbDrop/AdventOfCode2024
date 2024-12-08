50211a9ecd3bfa3138425cda1df5dca0089319b0
EbbDrop <ebbesteenhoudt@gmail.com>
2024-12-08 22:58:12 +0100

    Do new tricks on new version

--- src/day8.rs
+++ src/day8.rs
@@ -10,30 +10,29 @@ const SIZE: i32 = 12;
 
 const SIZE1: i32 = SIZE + 1;
 
-/// Has the `SIZE` lsb set
-const FIELD_SIZE: u64 = 2u64.pow(SIZE as u32) - 1;
-
 const FREQ_RANGE: usize = (b'z' - b'0' + 1) as usize;
 
 #[aoc(day8, part1)]
-pub fn part1(s: &str) -> u32 {
+pub fn part1(s: &str) -> u64 {
     unsafe { part1_inner(s) }
 }
 
-unsafe fn part1_inner(s: &str) -> u32 {
+unsafe fn part1_inner(s: &str) -> u64 {
     #[cfg(not(test))]
     const SIZE: i16 = 50;
     #[cfg(test)]
     const SIZE: i16 = 12;
 
     const SIZE1: i16 = SIZE + 1;
-
     let s = s.as_bytes();
 
-    let mut masts: [ArrayVec<[(i16, i16, u64); 3]>; FREQ_RANGE] =
-        [ArrayVec::from_array_empty([(0, 0, 0); 3]); FREQ_RANGE];
+    let mut masts: [ArrayVec<[i16; 3]>; FREQ_RANGE] =
+        [ArrayVec::from_array_empty([0; 3]); FREQ_RANGE];
 
-    let mut antinodes = [0u64; SIZE as usize];
+    let mut antinodes = [false; (SIZE * SIZE) as usize];
+    let mut total_antinotedes = 0;
+
+    // let mut numbers = [0; 5];
 
     for i in unsafe { OneInv::new_unchecked(b'.').iter(s) } {
         if s[i] == b'\n' {
@@ -45,42 +44,42 @@ unsafe fn part1_inner(s: &str) -> u32 {
         let new_x = i % SIZE1;
         let new_y = i / SIZE1;
 
-        let new_field: u64 = 1 << new_x;
-
         // numbers[masts[f as usize].len()] += 1;
 
-        for (mast_y, mast_x, mast_field) in masts.get_unchecked(f as usize) {
+        for mast_i in &masts[f as usize] {
+            let mast_x = mast_i % SIZE1;
+            let mast_y = mast_i / SIZE1;
+
             let diff_x = mast_x - new_x;
             let diff_y = new_y - mast_y;
 
-            if *mast_y >= diff_y {
+            let node_x = mast_x + diff_x;
+            if node_x >= 0 && node_x < SIZE && mast_y >= diff_y {
                 let node_y = mast_y - diff_y;
 
-                *antinodes.get_unchecked_mut(node_y as usize) |= if diff_x.is_positive() {
-                    mast_field << diff_x
-                } else {
-                    mast_field >> -diff_x
-                };
+                total_antinotedes +=
+                    !antinodes.get_unchecked((node_y * SIZE + node_x) as usize) as u64;
+                *antinodes.get_unchecked_mut((node_y * SIZE + node_x) as usize) = true;
             }
 
-            if new_y + diff_y < SIZE {
+            let node_x = new_x - diff_x;
+            if node_x >= 0 && node_x < SIZE && new_y + diff_y < SIZE {
                 let node_y = new_y + diff_y;
 
-                *antinodes.get_unchecked_mut(node_y as usize) |= if diff_x.is_positive() {
-                    new_field >> diff_x
-                } else {
-                    new_field << -diff_x
-                };
+                total_antinotedes +=
+                    !antinodes.get_unchecked((node_y * SIZE + node_x) as usize) as u64;
+                *antinodes.get_unchecked_mut((node_y * SIZE + node_x) as usize) = true;
             }
         }
 
-        masts[f as usize].try_push((new_y, new_x, new_field));
+        masts[f as usize].try_push(i);
     }
 
-    antinodes
-        .iter()
-        .map(|field| (field & FIELD_SIZE).count_ones())
-        .sum()
+    // for i in 0..5 {
+    //     println!("{i}: {}", numbers[i]);
+    // }
+
+    total_antinotedes
 }
 
 #[aoc(day8, part2)]
