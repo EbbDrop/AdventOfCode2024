f81535c2b43b72e20bc2e10286b5ffc1f5f141df
EbbDrop <ebbesteenhoudt@gmail.com>
2024-12-10 12:07:12 +0100

    Remove all arrays from part 1

--- src/day9.rs
+++ src/day9.rs
@@ -16,54 +16,56 @@ pub fn part1(s: &str) -> u64 {
     }
 }
 
+#[inline(always)]
+fn get_checksum(block_id: usize, position: u32, size: u32) -> u64 {
+    size as u64 * block_id as u64 * (2 * position as u64 + size as u64 - 1)
+}
+
 fn part1_inner(s: &str) -> u64 {
-    let input_map = s
-        .as_bytes()
-        .strip_suffix(&[b'\n'])
-        .unwrap_or_else(|| s.as_bytes());
+    let s = s.as_bytes();
 
-    // All blocks with their id, and empty space
-    let mut full_input_blocks: [u32; MAX_BLOCKS_SIZE] = [0; MAX_BLOCKS_SIZE];
-    // Like full but without empty space
-    let mut input_blocks: [u32; MAX_BLOCKS_SIZE / 2] = [0; MAX_BLOCKS_SIZE / 2];
+    let mut front_pointer = 0;
+    let mut back_pointer = INPUT_SIZE - 1;
 
-    let mut block_id = 0;
-    let mut full_input_blocks_i: usize = 0;
-    let mut input_blocks_i: usize = 0;
+    let mut sum = 0;
 
-    for c in input_map.chunks(2) {
-        let block_size = (c[0] - b'0') as usize;
+    let mut position = 0;
 
-        full_input_blocks[full_input_blocks_i..full_input_blocks_i + block_size].fill(block_id);
-        input_blocks[input_blocks_i..input_blocks_i + block_size].fill(block_id);
+    let mut to_move_size = 0;
+    let mut to_move_id = 0;
 
-        full_input_blocks_i += block_size;
-        input_blocks_i += block_size;
+    while front_pointer <= back_pointer {
+        let block_size = (s[front_pointer] - b'0') as u32;
+        front_pointer += 1;
 
-        if c.len() == 2 {
-            let empty_size = (c[1] - b'0') as usize;
-            full_input_blocks_i += empty_size;
-        }
+        sum += get_checksum(front_pointer / 2, position, block_size);
+        position += block_size;
 
-        block_id += 1;
-    }
+        let mut empty_size = (s[front_pointer] - b'0') as u32;
+        front_pointer += 1;
 
-    let full_size = full_input_blocks_i;
-    let empty_space = full_size - input_blocks_i;
-    let mut full_input_blocks_i: usize = (input_map[0] - b'0') as usize;
+        while empty_size > 0 {
+            if to_move_size == 0 {
+                to_move_size = (s[back_pointer] - b'0') as u32;
+                to_move_id = back_pointer / 2;
+                back_pointer -= 2;
+            }
 
-    for i in (input_blocks_i - empty_space - 1..input_blocks_i).rev() {
-        full_input_blocks[full_input_blocks_i] = input_blocks[i];
-        while full_input_blocks[full_input_blocks_i] != 0 {
-            full_input_blocks_i += 1;
+            let move_size = to_move_size.min(empty_size);
+            sum += get_checksum(to_move_id, position, move_size);
+            position += move_size;
+
+            to_move_size -= move_size;
+            empty_size -= move_size;
         }
     }
+    sum += get_checksum(to_move_id, position, to_move_size);
 
-    full_input_blocks[..full_size - empty_space]
-        .iter()
-        .enumerate()
-        .map(|(i, id)| i as u64 * *id as u64)
-        .sum()
+    println!("");
+    dbg!(to_move_id, to_move_size);
+    dbg!(front_pointer, back_pointer);
+
+    sum / 2
 }
 
 #[aoc(day9, part2)]
