a3db73493949c090290bc4e34c155f3411329348
EbbDrop <ebbesteenhoudt@gmail.com>
2024-12-10 13:34:21 +0100

    Inprove part 2

--- src/day9.rs
+++ src/day9.rs
@@ -5,9 +5,6 @@ const INPUT_SIZE: usize = 19999;
 #[cfg(test)]
 const INPUT_SIZE: usize = 19;
 
-// Assume a avrage of 5
-const MAX_BLOCKS_SIZE: usize = INPUT_SIZE * 5;
-
 #[aoc(day9, part1)]
 pub fn part1(s: &str) -> u64 {
     #[expect(unused_unsafe)]
@@ -18,6 +15,14 @@ pub fn part1(s: &str) -> u64 {
 
 #[inline(always)]
 fn get_checksum(block_id: usize, position: u32, size: u32) -> u64 {
+    // println!(
+    //     "at {}, got: \"{}\" ({})",
+    //     position,
+    //     std::iter::repeat((block_id as u8 + b'0') as char)
+    //         .take(size as usize)
+    //         .collect::<String>(),
+    //     size
+    // );
     size as u64 * block_id as u64 * (2 * position as u64 + size as u64 - 1)
 }
 
@@ -73,65 +78,46 @@ pub fn part2(s: &str) -> u64 {
 }
 
 fn part2_inner(s: &str) -> u64 {
-    let input_map = s
-        .as_bytes()
-        .strip_suffix(&[b'\n'])
-        .unwrap_or_else(|| s.as_bytes());
+    let s = s.as_bytes();
 
-    // All blocks with their id, and empty space
-    let mut full_input_blocks: [u32; MAX_BLOCKS_SIZE] = [0; MAX_BLOCKS_SIZE];
-    // Like full but without empty space
-    let mut input_blocks: [(usize, u32); INPUT_SIZE / 2 + 1] = [(0, 0); INPUT_SIZE / 2 + 1];
+    let mut sizes = [0; INPUT_SIZE];
 
-    let mut block_id = 0;
-    let mut full_input_blocks_i: usize = 0;
-    let mut input_blocks_i: usize = 0;
-
-    for c in input_map.chunks(2) {
-        let block_size = (c[0] - b'0') as usize;
-
-        full_input_blocks[full_input_blocks_i..full_input_blocks_i + block_size].fill(block_id);
-
-        input_blocks[input_blocks_i] = (full_input_blocks_i, block_size as u32);
-
-        full_input_blocks_i += block_size;
-        input_blocks_i += 1;
-
-        if c.len() == 2 {
-            let empty_size = (c[1] - b'0') as usize;
-            full_input_blocks_i += empty_size;
-        }
-
-        block_id += 1;
+    for i in 0..INPUT_SIZE {
+        sizes[i] = s[i] - b'0';
     }
 
-    let full_input_blocks_start: usize = (input_map[0] - b'0') as usize;
+    let mut i = INPUT_SIZE - 1;
 
-    for block_id in (1..input_blocks_i).rev() {
-        let (position, size) = input_blocks[block_id];
+    let mut sum = 0;
+    loop {
+        let block_size = s[i] - b'0';
 
-        let mut i = full_input_blocks_start;
-        let mut search_size = 0;
-        while i < position {
-            if full_input_blocks[i as usize] == 0 {
-                search_size += 1;
-                if search_size == size {
-                    full_input_blocks[position..position + size as usize].fill(0);
-                    full_input_blocks[i - size as usize + 1..i + 1].fill(block_id as u32);
-                    break;
-                }
-            } else {
-                search_size = 0;
+        let mut empty_pointer = 1;
+        let mut position = 0;
+        while empty_pointer < i {
+            position += sizes[empty_pointer - 1] as u32;
+
+            let empty_size = sizes[empty_pointer];
+            if empty_size >= block_size {
+                sizes[empty_pointer as usize] -= block_size;
+                sizes[empty_pointer as usize - 1] += block_size;
+
+                sum += get_checksum(i / 2, position, block_size as u32);
+                break;
             }
-            i += 1;
+            position += empty_size as u32;
+            empty_pointer += 2;
         }
+        if empty_pointer >= i {
+            sum += get_checksum(i / 2, position, block_size as u32);
+        }
+        if i == 0 {
+            break;
+        }
+        i -= 2;
     }
 
-    full_input_blocks
-        .iter()
-        .enumerate()
-        .map(|(i, id)| i as u64 * *id as u64)
-        .sum()
+    sum / 2
 }
 
 #[cfg(test)]
@@ -151,3 +137,14 @@ mod tests {
         assert_eq!(part2(EXAMPLE), 2858);
     }
 }
+
+// at 00, got: "00" (2)
+// at 02, got: "99" (2)
+// at 04, got: "2" (1)
+// at 05, got: "111" (3)
+// at 08, got: "777" (3)
+// at 12, got: "44" (2)
+// at 15, got: "333" (3)
+// at 22, got: "5555" (4)
+// at 27, got: "6666" (4)
+// at 36, got: "8888" (4)
