2ce7bed069c87a174348dca88ca1f179efcc11e5
EbbDrop <ebbesteenhoudt@gmail.com>
2024-12-22 00:57:06 +0100

    Inline asm

--- src/day21.rs
+++ src/day21.rs
@@ -234,99 +234,106 @@ static LUT_P1: [u64; 10usize.pow(3)] = const {
 static LUT_P2: [u64; 10usize.pow(3)] =
     unsafe { transmute(*include_bytes!(concat!(env!("OUT_DIR"), "/day21.bin"))) };
 
-use std::arch::x86_64::*;
-
-// 029An980An179An4 56An379A
-
-// 029An980An179An4 56An379A
-
-// 029A n980 An17 9An4 56An 379A
-#[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
-unsafe fn inner(s: &str, lut: &[u64; 1000]) -> u64 {
-    // 029An980An179An456An379A
-    let v: __m256i = s.as_ptr().cast::<__m256i>().read_unaligned();
-    let v = _mm256_subs_epu8(v, _mm256_set1_epi8(b'0' as i8));
-
-    // 029A n980 0000 0000 An17 9An4 56An 379A
-    let v = _mm256_permutevar8x32_epi32(v, _mm256_setr_epi32(0, 1, 6, 6, 2, 3, 4, 5));
-
-    // _029_980_________ _179_456____ _379
-    let idx = _mm256_setr_epi8(
-        15, 0, 1, 2, 15, 5, 6, 7, //
-        15, 15, 15, 15, 15, 15, 15, 15, //
-        15, 2, 3, 4, 15, 7, 8, 9, //
-        15, 15, 15, 15, 15, 12, 13, 14,
-    );
-    let v = _mm256_shuffle_epi8(v, idx);
-
-    // 0, 29, 9, 80, 0, 0, 0, 0, 1, 79, 4, 56, 0, 0, 7, 90
-    let mul: __m256i = _mm256_setr_epi8(
-        0, 1, 10, 1, 0, 1, 10, 1, //
-        0, 0, 0, 0, 0, 0, 0, 0, //
-        0, 1, 10, 1, 0, 1, 10, 1, //
-        0, 0, 0, 0, 0, 1, 10, 1, //
-    );
-    let v = _mm256_maddubs_epi16(v, mul);
-
-    // 29, 980, 0, 0, 179, 456, 0, 379
-    let mul: __m256i = _mm256_setr_epi16(
-        100, 1, 100, 1, //
-        0, 0, 0, 0, //
-        100, 1, 100, 1, //
-        0, 0, 100, 1,
-    );
-    let v = _mm256_madd_epi16(v, mul);
-    *lut.get_unchecked(_mm256_extract_epi32::<0>(v) as u32 as usize)
-        + *lut.get_unchecked(_mm256_extract_epi32::<1>(v) as u32 as usize)
-        + *lut.get_unchecked(_mm256_extract_epi32::<4>(v) as u32 as usize)
-        + *lut.get_unchecked(_mm256_extract_epi32::<5>(v) as u32 as usize)
-        + *lut.get_unchecked(_mm256_extract_epi32::<7>(v) as u32 as usize)
-    // let com_l = _mm256_i32gather_epi32::<8>(lut.as_ptr().cast(), v);
-    // let com_h = _mm256_i32gather_epi32::<8>(lut.as_ptr().cast::<i32>().offset(1), v);
-
-    // let h1 = _mm256_unpacklo_epi32(com_l, com_h);
-    // let h2 = _mm256_unpackhi_epi32(com_l, com_h);
-
-    // let sum = 0;
-    // let v = _mm256_add_epi64(h1, h2);
-    // let vs = _mm256_shuffle_epi32::<{ (1 << 6) | (0 << 4) | (3 << 2) | 2 }>(v);
-    // let v = _mm256_add_epi64(v, vs);
-
-    // _mm256_extract_epi64::<0>(v) as u64 + _mm256_extract_epi64::<2>(v) as u64
-    // let mut p = [0u32; 256 / 32];
-    // p.as_mut_ptr().cast::<__m256i>().write(v);
-    // println!("{:?}", &p);
-
-    // panic!()
-
-    // let v = _mm256_shuffle_epi8(v, )
-
-    // let s = s.as_bytes();
-    // let mut sum = 0;
-    // unsafe {
-    //     let mut i = 0;
-    //     for _ in 0..5 {
-    //         let idx = (*s.get_unchecked(i + 0) as usize) * 100
-    //             + (*s.get_unchecked(i + 1) as usize) * 10
-    //             + (*s.get_unchecked(i + 2) as usize) * 1
-    //             - (b'0' as usize * 111);
-
-    //         sum += lut[idx];
-    //         i += 5;
-    //     }
-    // }
-
-    // sum
-}
-
 #[aoc(day21, part1)]
 pub fn part1(s: &str) -> u64 {
-    unsafe { inner(s, &LUT_P1) }
+    static LCPI0_0: [u8; 32] = [
+        3, 8, 9, 10, 3, 13, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0, 7, 10, 11, 12, 7, 15, 0, 1, 0, 0, 0, 0,
+        7, 4, 5, 6,
+    ];
+    static LCPI0_1: [u8; 32] = [
+        48, 48, 48, 48, 48, 48, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0, 48, 48, 48, 48, 48, 48, 48, 48, 0,
+        0, 0, 0, 48, 48, 48, 48,
+    ];
+    static LCPI0_2: [u8; 32] = [
+        0, 1, 10, 1, 0, 1, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 10, 1, 0, 1, 10, 1, 0, 0, 0, 0, 0,
+        1, 10, 1,
+    ];
+    static LCPI0_3: [u16; 16] = [100, 1, 100, 1, 0, 0, 0, 0, 100, 1, 100, 1, 0, 0, 100, 1];
+
+    let r: u64;
+    unsafe {
+        std::arch::asm!(
+            "vpermq          {ymm:y}, ymmword ptr [{s}], 99",
+            "vpshufb         {ymm:y}, {ymm:y}, ymmword ptr [rip + {LCPI0_0}]",
+            "vpsubusb        {ymm:y}, {ymm:y}, ymmword ptr [rip + {LCPI0_1}]",
+            "vpmaddubsw      {ymm:y}, {ymm:y}, ymmword ptr [rip + {LCPI0_2}]",
+            "vpmaddwd        {ymm:y}, {ymm:y}, ymmword ptr [rip + {LCPI0_3}]",
+            "vmovd           {c:e}, {ymm:x}",
+            "vpextrd         {a:e}, {ymm:x}, 1",
+            "mov             {a:r}, qword ptr [{lut} + 8*{a:r}]",
+            "add             {a:r}, qword ptr [{lut} + 8*{c:r}]",
+            "vextracti128    {ymm:x}, {ymm:y}, 1",
+            "vmovd           {c:e}, {ymm:x}",
+            "add             {a:r}, qword ptr [{lut} + 8*{c:r}]",
+            "vpextrd         {c:e}, {ymm:x}, 1",
+            "add             {a:r}, qword ptr [{lut} + 8*{c:r}]",
+            "vpextrd         {c:e}, {ymm:x}, 3",
+            "add             {a:r}, qword ptr [{lut} + 8*{c:r}]",
+
+            LCPI0_0 = sym LCPI0_0,
+            LCPI0_1 = sym LCPI0_1,
+            LCPI0_2 = sym LCPI0_2,
+            LCPI0_3 = sym LCPI0_3,
+            s = in(reg) s.as_ptr(),
+            lut = in(reg) LUT_P1.as_ptr(),
+            ymm = out(ymm_reg) _,
+            c = out(reg) _,
+            a = out(reg) r,
+            options(nostack)
+        );
+    }
+    r
 }
 
 #[aoc(day21, part2)]
 pub fn part2(s: &str) -> u64 {
-    unsafe { inner(s, &LUT_P2) }
+    static LCPI0_0: [u8; 32] = [
+        3, 8, 9, 10, 3, 13, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0, 7, 10, 11, 12, 7, 15, 0, 1, 0, 0, 0, 0,
+        7, 4, 5, 6,
+    ];
+    static LCPI0_1: [u8; 32] = [
+        48, 48, 48, 48, 48, 48, 48, 48, 0, 0, 0, 0, 0, 0, 0, 0, 48, 48, 48, 48, 48, 48, 48, 48, 0,
+        0, 0, 0, 48, 48, 48, 48,
+    ];
+    static LCPI0_2: [u8; 32] = [
+        0, 1, 10, 1, 0, 1, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 10, 1, 0, 1, 10, 1, 0, 0, 0, 0, 0,
+        1, 10, 1,
+    ];
+    static LCPI0_3: [u16; 16] = [100, 1, 100, 1, 0, 0, 0, 0, 100, 1, 100, 1, 0, 0, 100, 1];
+
+    let r: u64;
+    unsafe {
+        std::arch::asm!(
+            "vpermq          {ymm}, ymmword ptr [{s}], 99",
+            "vpshufb         {ymm}, {ymm}, ymmword ptr [rip + {LCPI0_0}]",
+            "vpsubusb        {ymm}, {ymm}, ymmword ptr [rip + {LCPI0_1}]",
+            "vpmaddubsw      {ymm}, {ymm}, ymmword ptr [rip + {LCPI0_2}]",
+            "vpmaddwd        {ymm}, {ymm}, ymmword ptr [rip + {LCPI0_3}]",
+            "vmovd           {t:e}, {ymm:x}",
+            "vpextrd         {r:e}, {ymm:x}, 1",
+            "mov             {r:r}, qword ptr [{lut} + 8*{r:r}]",
+            "add             {r:r}, qword ptr [{lut} + 8*{t:r}]",
+            "vextracti128    {ymm:x}, {ymm}, 1",
+            "vmovd           {t:e}, {ymm:x}",
+            "add             {r:r}, qword ptr [{lut} + 8*{t:r}]",
+            "vpextrd         {t:e}, {ymm:x}, 1",
+            "add             {r:r}, qword ptr [{lut} + 8*{t:r}]",
+            "vpextrd         {t:e}, {ymm:x}, 3",
+            "add             {r:r}, qword ptr [{lut} + 8*{t:r}]",
+
+            LCPI0_0 = sym LCPI0_0,
+            LCPI0_1 = sym LCPI0_1,
+            LCPI0_2 = sym LCPI0_2,
+            LCPI0_3 = sym LCPI0_3,
+            s = in(reg) s.as_ptr(),
+            lut = in(reg) LUT_P2.as_ptr(),
+            r = out(reg) r,
+            ymm = out(ymm_reg) _,
+            t = out(reg) _,
+            options(nostack)
+        );
+    }
+    r
 }
 
 #[cfg(test)]
