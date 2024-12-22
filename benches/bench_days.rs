fbea5fcf30d7a49c6612a9d0b470c29bd0a774cf
EbbDrop <ebbesteenhoudt@gmail.com>
2024-12-22 01:13:40 +0100

    More closly resembly real benching setup

--- benches/bench_days.rs
+++ benches/bench_days.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use paste::paste;
+use std::{fmt::Display, hint::black_box};
 
 /// Get input for a single day
 macro_rules! get_day_input {
@@ -17,8 +18,17 @@ macro_rules! benches_day {
             pub fn [<bench_day $day_num>](c: &mut Criterion) {
                 let mut group = c.benchmark_group(concat!("day", $day_num));
                 let input = get_day_input!($day_num);
-                group.bench_function(format!("day{}_part1", $day_num), |b| b.iter(|| [<day $day_num>]::part1(input)));
-                group.bench_function(format!("day{}_part2", $day_num), |b| b.iter(|| [<day $day_num>]::part2(input)));
+
+                #[inline(never)]
+                fn routine_part1(input: &str) -> impl Display + '_ {
+                    [<day $day_num>]::part1(black_box(input))
+                }
+                group.bench_with_input(format!("day{}_part1", $day_num), input, |b, i| b.iter(|| routine_part1(i)));
+                #[inline(never)]
+                fn routine_part2(input: &str) -> impl Display + '_ {
+                    [<day $day_num>]::part2(black_box(input))
+                }
+                group.bench_with_input(format!("day{}_part2", $day_num), input, |b, i| b.iter(|| routine_part2(i)));
             }
         }
     };
